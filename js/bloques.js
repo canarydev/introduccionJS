427780aa5b45b9f51c33a325da4906153ff89c41
Joshua M. González Santana <canary_styl3@hotmail.com>
2020-02-13 09:26:50 +0000

    Se crea bloques

---
+++ js/bloques.js
@@ -0,0 +1,34 @@
+/* ===============
+   B L O Q U E S
+ =================
+
+Los bloques pueden ser de varios tipos, aunque se pueden simplificar en dos grupos; sentencias condicionales y bucles.
+*/
+
+var i = 0;
+
+/* SENTENCIAS CONDICIONALES */
+
+/*================
+        I F
+==================
+
+Ejecuta el código si se cumple la condición.
+
+Estructura:
+    
+    if (condición) {
+        código;
+    }
+    
+También puede verse de esta otra forma, siempre que el código que se ejecute si se cumple la condición sea de una sola línea:
+    
+    if (condición) código;
+
+*/
+if (i == 0) {
+    console.log ("Hola");
+}
+
+if (true) console.log("mundo");
+
